fb4ce55abfe2b06a1dfbc09ab6f9c7a7dadfee5d
muhafrom <83685640+muhafrom@users.noreply.github.com>
2025-03-04 17:26:10 +0200

    Update commonRejectionService.ts

--- src/services/common_rejections/commonRejectionService.ts
+++ src/services/common_rejections/commonRejectionService.ts
@@ -20,6 +20,28 @@ export const createCommonRejection = async (data: {
   });
 };
 
+export const createCommonRejections = async (
+  data: Array<{
+    common_rejection_type: common_rejection_type;
+    resolution_process: string;
+  }>
+) => {
+  return prisma.$transaction(async (prisma) => {
+    const commonRejections = [];
+
+    for (const rejectionData of data) {
+      const commonRejection = await prisma.common_rejection.create({
+        data: rejectionData,
+      });
+
+      commonRejections.push(commonRejection);
+    }
+
+    return commonRejections;
+  });
+};
+
+
 export const updateCommonRejection = async (id: string, data: any) => {
   return prisma.common_rejection.update({
     where: { common_rejection_id: id },
