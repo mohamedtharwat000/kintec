f50ab61726e43f7b8a9b4bdbfc86eba17bf5f932
muhafrom <83685640+muhafrom@users.noreply.github.com>
2025-03-04 17:18:20 +0200

    Update projectService.ts

--- src/services/projects/projectService.ts
+++ src/services/projects/projectService.ts
@@ -21,6 +21,30 @@ export const createProject = async (data: {
   });
 };
 
+export const createProjects = async (
+  data: Array<{
+    project_name: string;
+    project_type: string;
+  }>
+) => {
+  return prisma.$transaction(async (prisma) => {
+    const projects = [];
+
+    for (const projectData of data) {
+      const project = await prisma.project.create({
+        data: {
+          ...projectData,
+        },
+      });
+
+      projects.push(project);
+    }
+
+    return projects;
+  });
+};
+
+
 export const updateProject = async (id: string, data: any) => {
   return prisma.project.update({
     where: { project_id: id },
