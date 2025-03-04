c0e96ff1d5a93d76c4c607f705121bc0c8df8bae
muhafrom <83685640+muhafrom@users.noreply.github.com>
2025-03-04 17:25:08 +0200

    Update contractService.ts

--- src/services/contracts/contractService.ts
+++ src/services/contracts/contractService.ts
@@ -58,6 +58,38 @@ export const createContract = async (data: {
   });
 };
 
+export const createContracts = async (data: {
+  contractor_id: string;
+  client_company_id: string;
+  project_id: string;
+  contract_start_date: string;
+  contract_end_date: string;
+  job_title: string;
+  job_type: string;
+  job_number: string;
+  kintec_cost_centre_code: string;
+  description_of_services: string;
+  contract_status: contract_status;
+}) => {
+  return prisma.contract.create({
+    data: {
+      contractor: { connect: { contractor_id: data.contractor_id } },
+      client_company: {
+        connect: { client_company_id: data.client_company_id },
+      },
+      project: { connect: { project_id: data.project_id } },
+      contract_start_date: new Date(data.contract_start_date),
+      contract_end_date: new Date(data.contract_end_date),
+      job_title: data.job_title,
+      job_type: data.job_type,
+      job_number: data.job_number,
+      kintec_cost_centre_code: data.kintec_cost_centre_code,
+      description_of_services: data.description_of_services,
+      contract_status: data.contract_status,
+    },
+  });
+};
+
 export const updateContract = async (id: string, data: any) => {
   const {
     contractor_id,
