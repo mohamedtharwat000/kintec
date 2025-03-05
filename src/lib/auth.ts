ca6ccc574fcfaef5a3fae90e8a5a2365b1884f7a
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2025-03-05 11:04:35 +0200

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,18 +8,6 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         username: { label: "Username", type: "text" },
         password: { label: "Password", type: "password" },
       },
-      authorize: async (credentials) => {
-        const { username, password } = credentials as {
-          username: string;
-          password: string;
-        };
-        console.log(credentials);
-
-        if (username === "criticalfuture" && password === "password") {
-          return { id: "1", name: "criticalfuture", username };
-        }
-        throw new Error("Invalid credentials.");
-      },
     }),
   ],
 });
